5ae78f79da4601d7924202cf70c8ea5d4d014058
Varunkumar Nagarajan <varunkumar.n@gmail.com>
2021-06-08 23:46:03 +0530

    Update login and logout methods

--- index.js
+++ index.js
@@ -1,11 +1,41 @@
 import puppeteer from 'puppeteer';
+import { PASSWORD, USER_NAME } from './creds.js';
+import { sleep } from './utils.js';
+
+const login = async (page) => {
+  const USER_NAME_SELECTOR = '#LoginId';
+  const PASSWORD_SELECTOR = '#LoginPassword';
+  const SUBMIT_SELECTOR = '#btnSignIn';
+
+  await page.click(USER_NAME_SELECTOR);
+  await page.keyboard.type(USER_NAME);
+
+  await page.click(PASSWORD_SELECTOR);
+  await page.keyboard.type(PASSWORD);
+
+  await page.click(SUBMIT_SELECTOR);
+
+  await page.waitForNavigation();
+};
+
+const logout = async (page) => {
+  await page.goto('https://www.parentsalarm.com/About/LogOut');
+};
 
 const run = async () => {
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({
+    headless: false,
+  });
   const page = await browser.newPage();
+  await page.goto('https://www.parentsalarm.com/', {
+    waitUntil: 'domcontentloaded',
+  });
 
-  await page.goto('https://www.parentsalarm.com/');
+  await login(page);
 
+  await sleep(1000);
+
+  await logout(page);
   browser.close();
 };
 

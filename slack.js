32771f9fed1f6178cda80b5d398ae9382690a06f
Varunkumar Nagarajan <varunkumar.n@gmail.com>
2021-10-04 12:32:40 +0530

    Stop swallowing of slack exceptions

--- slack.js
+++ slack.js
@@ -3,15 +3,11 @@ import { WebClient } from '@slack/web-api';
 const web = new WebClient(process.env.SLACK_OAUTH_TOKEN);
 
 const sendMessage = async (message, channel = '#parentsalarm') => {
-  try {
-    await web.chat.postMessage({
-      channel,
-      text: message,
-    });
-    console.log('Message posted!');
-  } catch (error) {
-    console.log(error);
-  }
+  await web.chat.postMessage({
+    channel,
+    text: message,
+  });
+  console.log('Message posted!');
 };
 
 export default sendMessage;
